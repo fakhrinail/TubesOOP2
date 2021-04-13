5d38531e471c95da1114f96c35abbcd6db78a387
arsa-dg <arsaadg891@gmail.com>
2021-04-13 23:54:50 +0700

    add parent in engimon

--- Engimon.java
+++ Engimon.java
@@ -1,26 +1,27 @@
+import java.util.ArrayList;
+
 public class Engimon{
   private String species; private String name;
   private String interaction;
-  // private String parent1Species;
-  // private String parent1Name;
-  // private String parent2Species;
-  // private String parent2Name; buat array
+  private ArrayList<String> parentNames;
+  private ArrayList<String> parentSpecies;
   private int countSkill; private int countElement;
-  // ArrayList<> skill;
-  // ArrayList<> elements;
+  private ArrayList<String> skill;
+  private ArrayList<String> elements;
   private int life; private int level;
   private int experience; private int cumulativeExperience;
   private int posX; private int posY;
+  private final int maxCumulativeExperience = 5000; //5000?
 
   public Engimon(String species, String name, String parent1Species, 
   String parent1Name, String parent2Species, String parent2Name, 
   String interaction, int life, int level){
     this.species = species; this.name = name;
     this.interaction = interaction;
-    // this.parentName = new String[2];
-    // this.parentSpecies = new String[2];
-    // this.parentName[0] = parentName; this->parentName[1] = parentName2;
-    // this.parentSpecies[0] = parentSpecies; this->parentSpecies[1] = parentSpecies2;
+    this.parentNames = new ArrayList<String>(2);
+    this.parentNames.add(parent1Name); this.parentNames.add(parent2Name);
+    this.parentSpecies = new ArrayList<String>(2);
+    this.parentSpecies.add(parent1Species); this.parentSpecies.add(parent2Species);
     // this.learnedSkills = new Skill[4];
     // this.elements = new Element[3];
     this.life = life; this.level = level;
@@ -66,12 +67,16 @@ public class Engimon{
   }
 
   public void addExperience(int exp){
-    this.experience += exp; this.cumulativeExperience += exp;
-    if (this.experience > 100){
-      int addLevel = this.experience/100;
-      for (int i=0; i<addLevel; i++){
-        levelUp();
+    if (this.cumulativeExperience + exp <= 5000){
+      this.experience += exp; this.cumulativeExperience += exp;
+      if (this.experience > 100){
+        int addLevel = this.experience/100;
+        for (int i=0; i<addLevel; i++){
+          levelUp();
+        }
       }
+    }else{
+      death();
     }
   }
 
@@ -81,16 +86,23 @@ public class Engimon{
   }
 
   public void death(){
+    // harusnya gui
     System.out.println("Engimon "+this.name+" mati.");
   }
 
   public void showDetail(){
+    // harusnya gui
     System.out.println("Species "+this.species);
     System.out.println("Nama "+this.name);
     System.out.println("Life "+this.life+"/3");
+    for (int i=0; i<2; i++){
+      System.out.println("Parent "+(i+1));
+      System.out.println("Nama: "+this.parentNames.get(i));
+      System.out.println("Species "+this.parentSpecies.get(i));
+    }
     System.out.println("Level "+this.level);
-    System.out.println("Experience "+this.experience);
-    System.out.println("Cumulative Experience "+this.cumulativeExperience+"/5000??");
+    System.out.println("Experience "+this.experience+"/100");
+    System.out.println("Cumulative Experience "+this.cumulativeExperience+"/5000");
   }
 
 }
--- EngimonDriver.java
+++ EngimonDriver.java
@@ -1,6 +1,9 @@
 class EngimonDriver{
 	public static void main(String[] args){
     Engimon aaa = new Engimon("a", "b", "c", "d", "e", "f", "g", 1, 1);
+    aaa.addExperience(2069);
+    aaa.showDetail();
+    aaa.addExperience(5000);
     aaa.showDetail();
   }
 }
