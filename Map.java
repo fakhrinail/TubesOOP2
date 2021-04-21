9f5a992260e49ac948d29d07f5c2f0e44d6eac06
hafid abi d <aselole87@gmail.com>
2021-04-22 00:49:26 +0700

    map masih awalan

---
+++ Map.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.cobaguitubes;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author abida
+ */
+public abstract class Map {
+    public static char Tundra='1';
+    public static char Sea='2';
+    public static char Grassland='3';
+    public static char Mountain='4';
+    private final String filename;
+    ArrayList<ArrayList<Integer>> mapLayout; 
+    
+    public Map(String filename){
+        this.filename = filename;
+        this.loadMap();
+    }
+    
+    public static final void setMapIcon(char tundra, char sea, char grassLand, char mountain){
+        Tundra = tundra;
+        Sea = sea;
+        Grassland = grassLand;
+        Mountain = mountain;
+    }
+    
+    private Boolean loadMap(){
+        Boolean stats = true;
+        try{
+            File file = new File(filename);
+            BufferedReader buff = new BufferedReader(new FileReader(file));
+            String temp = buff.readLine();
+            while(temp != null){
+                System.out.println(temp);
+                temp = buff.readLine();
+            }
+        }catch(Exception e){
+            stats = false;
+        }finally{
+            return stats;
+        }
+    }
+    
+    
+    
+    
+    
+}
