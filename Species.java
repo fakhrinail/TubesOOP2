8df0a8e7a3ee960a0eea122a5c61e142a74ae796
arsa-dg <13519037@std.stei.itb.ac.id>
2021-04-24 11:52:46 +0700

    major changes on species and engimon

--- Species.java
+++ Species.java
@@ -6,6 +6,13 @@ public class Species {
     private ArrayList<Elemental> elementals; 
     private String interaction;
 
+    public Species(){
+        this.species = "Default";
+        this.skills = new ArrayList<Skill>();
+        this.elementals = new ArrayList<Elemental>();
+        this.interaction = "Default";
+    }
+    
     public Species(String[] SpeciesInfo, String interaction, KoleksiSkill allSkill){
         this.species = SpeciesInfo[0];
         this.skills = new ArrayList<Skill>();
@@ -16,6 +23,17 @@ public class Species {
         }
         this.interaction = interaction;
     }
+
+    public Species(String species, ArrayList<Skill> skills, ArrayList<Elemental> elementals, String interaction){
+        this.species = species;
+        this.skills = new ArrayList<Skill>();
+        this.skills.addAll(skills);
+        this.elementals = new ArrayList<Elemental>();
+        this.elementals.addAll(elementals);
+        this.interaction = interaction;
+    }
+
+    //Copy
     public Species(Species other){
         this.species = other.species;
         this.elementals = other.elementals;
@@ -25,6 +43,49 @@ public class Species {
         }
         this.interaction = other.interaction;
     }
+    
+    //Getters dan Setters
+    public String getSpecies(){
+        return this.species;
+    }
+
+    public ArrayList<Skill> getSkills(){
+        return this.skills;
+    }
+
+    public ArrayList<Elemental> getElementals(){
+        return this.elementals;
+    }
+
+    public String getInteraction(){
+        return this.interaction;
+    }
+
+    public void addSkill(Skill skill){
+        this.skills.add(skill);
+    }
+
+    public void addElemental(Elemental elemental){
+        this.elementals.add(elemental);
+    }
+    
+    public void setSpecies(String species){
+        this.species = species;
+    }
+
+    public void setInteraction(String interaction){
+        this.interaction = interaction;
+    }
+
+    public void setSkills(ArrayList<Skill> skills){
+        this.skills = skills;
+    }
+
+    public void setElementals(ArrayList<Elemental> elementals){
+        this.elementals = elementals;
+    }
+    
+    //Method lain
     public void printSpeciesDetail(){
         System.out.println(this.species);
         this.skills.get(0).printDetail();
