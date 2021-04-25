b51941fbc8653dd39c80e29c8f170176607dbe59
kinantanbagaspati <kinantanyolo@gmail.com>
2021-04-25 20:04:12 +0700

    ketiduran

--- GamePage.java
+++ GamePage.java
@@ -4,18 +4,20 @@ import javax.swing.*;
 
 public class GamePage extends JFrame{
     private JLabel[][] peta;
+    private JLabel player;
+    
     private JLayeredPane layeredPane;
     public void initComponent(){
 
         this.layeredPane = new JLayeredPane();
-        this.layeredPane.setBounds(0,0,500,500);
+        this.layeredPane.setBounds(0,0,750,550);
 
         this.peta = new JLabel[10][10];
         for(int i=0; i<10; i++){
             for(int j=0; j<10; j++){
                 this.peta[i][j] = new JLabel();
                 this.peta[i][j].setOpaque(true);
-                this.peta[i][j].setBounds(50*i, 50*j, 50, 50);
+                this.peta[i][j].setBounds(200 + 50*i, 50*j, 50, 50);
                 if((i+j)%4 == 0){
                     this.peta[i][j].setBackground(Color.CYAN);
                 }else if((i+j)%4 == 1){
@@ -30,7 +32,7 @@ public class GamePage extends JFrame{
         }
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(500,500);
+        this.setSize(750,550);
         this.add(this.layeredPane);
         
         this.setVisible(true);
